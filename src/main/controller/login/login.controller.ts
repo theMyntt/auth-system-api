7397174acde342b950eb51a0da3d3fb9a833f8e5
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-03-21 22:32:23 -0300

    feat: post method to login controller

--- src/main/controller/login/login.controller.ts
+++ src/main/controller/login/login.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { LoginService } from 'src/main/service';
 
-@Controller('login')
-export class LoginController {}
+@Controller('api')
+export class LoginController {
+  constructor(private readonly loginService: LoginService) {}
+
+  @Post('login')
+  login(@Body("email") email: string, @Body("password") password: string): string {
+    return this.loginService.getLogin(email, password);
+  }
+}

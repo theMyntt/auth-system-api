1186320fb3e7f8fe9a1a225113198a67a0445d44
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-03-21 23:01:47 -0300

    delete: unused archives

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { LoginModule } from './main/module';
 
 @Module({
   imports: [LoginModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}

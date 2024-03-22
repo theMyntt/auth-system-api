264fef066f3605ea0fc353b20d7a43c5e5453102
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-03-21 22:50:45 -0300

    feat: mongoDB connection

--- src/main/module/login/login.module.ts
+++ src/main/module/login/login.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserSchema } from 'src/auth';
 import { LoginController } from 'src/main/controller';
 import { LoginService } from 'src/main/service';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/User'),
+    MongooseModule.forFeature([{ name: "User", schema: UserSchema }])
+  ],
   controllers: [LoginController],
   providers: [LoginService],
 })

5fcbf251e1180bc978ae41d35cca970534b0d350
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-03-21 22:58:40 -0300

    feat: return login informations

--- src/main/service/login/login.service.ts
+++ src/main/service/login/login.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User } from 'src/auth/schemas/user.schema';
+import { HashText } from 'src/utils';
 
 @Injectable()
-export class LoginService {}
+export class LoginService {
+  constructor(@InjectModel("User") private readonly userModel: Model<User>) { }
+
+  async getLogin(email: string, password: string) {
+    const hashPassword: string = HashText(password);
+    
+    try {
+      const result = await this.userModel.findOne({email: email, password: hashPassword}).exec();
+
+      if (result) {
+        return result;
+      }
+
+      return "Não cadastrado";
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  }
+}

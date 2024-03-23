632e1dcf836dbb856896c507bdda1f7ce7b5613b
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-03-22 22:38:59 -0300

    feat: if have user, dont create another user

--- src/main/service/register/register.service.ts
+++ src/main/service/register/register.service.ts
@@ -5,26 +5,31 @@ import { User } from 'src/auth';
 import { HashText } from 'src/utils';
 
 interface Informations {
-  name: string,
-  email: string,
-  password: string
+  name: string;
+  email: string;
+  password: string;
 }
 @Injectable()
 export class RegisterService {
-  constructor(@InjectModel("User") private readonly userModel: Model<User>) {}
+  constructor(@InjectModel('User') private readonly userModel: Model<User>) {}
 
   async createUser(name: string, email: string, password: string) {
     const hashPassword = HashText(password);
     const information: Informations = {
       name: name,
       email: email,
-      password: hashPassword
-    }
+      password: hashPassword,
+    };
 
     try {
+      const hasUser = await this.userModel.findOne({email: email}).exec();
+      console.log(hasUser)
+
+      if (hasUser) return 'Usuário ja cadastrado.';
+
       await new this.userModel(information).save();
 
-      return "Usuário cadastrado com sucesso";
+      return 'Usuário cadastrado com sucesso';
     } catch (error) {
       throw new Error(error.message);
     }
